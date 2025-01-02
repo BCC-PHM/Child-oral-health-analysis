85b77423075e3cd8a652b0983f5358fbe9ebb68d
David.Ellis <david.ellis@birmingham.gov.uk>
2025-01-02 12:44:01 +0000

    Plotted dental outcome inequalities

--- python/oral-health-survey.py
+++ python/oral-health-survey.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 from textwrap import wrap
 from os.path import join
 
+import seaborn as sns
+custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+sns.set_theme(style="ticks", rc=custom_params)
+
 #%% Load and process data
 
 data = pd.read_excel(
@@ -32,14 +36,24 @@ data["any_dental_issue"] = list(data["Plaque"]) or \
     list(data["Incisor_Caries"]) or \
     list(data["PUFA_signs"])
     
-
-#%% Plot number surveyed
-
+#%% Define deprivation indicies and outcomes to be plotted
+    
+# Type : data column
 IMD_types = {
     "National" : "IMD19_National_Quintile",
     "LA" : "IMD19_LA_Quintile"
     }
 
+# data column : Plot title
+outcomes = {
+    "Plaque" : "% with Plaque\n(<1/3 Labial Surfaces)",
+    "Enamel_Caries" : "% with 1 or more\nEnamel Caries",
+    "Incisor_Caries" :"% with Incisor\nCaries",
+    "PUFA_signs" : "% with 1 or more\nPUFA signs",
+    "any_dental_issue" : "% with 1 or more\ndental issue"
+    }
+
+#%% Plot number surveyed
 for IMD_type in IMD_types.keys():
     count_pivot = Mat.get_pivot(
             data, 
@@ -49,15 +63,58 @@ for IMD_type in IMD_types.keys():
             ).astype(int)
     
     fig = Mat.inequality_map(count_pivot, 
-                       title = "Children\nSurveyed",
+                       title = "Children Surveyed",
                        supp_thresh=0,
                        CI_method = "Wilson",
-                       palette="Blues")
+                       palette="Blues",
+                       bar_rel_size = [0.25, 0.25])
     
     fig.axes[0].set_ylabel("IMD Quintile\n(" +IMD_type +")")
     fig.axes[0].set_xlabel("")
+    
+    # Save matrix
     save_name = join("..","output", "matricies",
                      IMD_type,"children_surveyed.png")
     fig.savefig(save_name, bbox_inches = "tight",
                     dpi = 300)
+plt.close("all")
+
+#%% Plot inequality matricices
+
+for IMD_type in IMD_types.keys():
+    for i, var in enumerate(outcomes.keys()):    
+        perc_pivot = Mat.get_pivot(
+                data, 
+                var,
+                eth_col = "Broad_Ethnicity", 
+                IMD_col = IMD_types[IMD_type],
+                mode="percentage"
+                ).fillna(0)
+    
+        count_pivot = Mat.get_pivot(
+                data, 
+                eth_col = "Broad_Ethnicity", 
+                IMD_col = IMD_types[IMD_type],
+                mode="count"
+                )
+        count_pivot[count_pivot == 0] = 0.1
+        
+        fig = Mat.inequality_map(count_pivot, 
+                           perc_pivot,
+                           title = outcomes[var],
+                           ttest = True,
+                           supp_thresh=0.2,
+                           supp_label = "No data",
+                           CI_method = "Wilson",
+                           palette="Blues",
+                           bar_rel_size = [0.25, 0.25])
+        fig.axes[0].set_ylabel("IMD Quintile\n(" +IMD_type +")")
+        fig.axes[0].set_xlabel("")
+        
+        # Save matrix
+        save_name = join("..","output", "matricies",
+                     IMD_type,var+".png")
+        fig.savefig(save_name, bbox_inches = "tight",
+                        dpi = 300)
+    break
 plt.close("all")
